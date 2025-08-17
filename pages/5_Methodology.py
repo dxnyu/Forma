7782b58ca22dd95757a2db7cf0fa25cc7ecb5e5e
dxnyu <142132712+dxnyu@users.noreply.github.com>
2025-08-17 11:29:58 +0800

    Change image path

--- pages/5_Methodology.py
+++ pages/5_Methodology.py
@@ -4,7 +4,7 @@ st.title("Methodology")
 st.write("The flowcharts below represent how the LLM generates responses in each feature")
 
 st.subheader("Set Up Your Business in Singapore")
-st.image("./Flowcharts/Set Up Your Business in Singapore.jpg"
+st.image("./Flowcharts/Set Up Your Business in Singapore.jpg")
 
 st.subheader("Find Government Support")
 st.image("https://drive.google.com/file/d/1dPOTzeTJS1TaD6LjxiLPZiKD2wTSk3uA/view?usp=drive_link")
