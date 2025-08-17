623425a6639c65970baa85dec6c0d476ae6feedf
dxnyu <142132712+dxnyu@users.noreply.github.com>
2025-08-17 11:30:56 +0800

    Change image paths

--- pages/5_Methodology.py
+++ pages/5_Methodology.py
@@ -7,7 +7,7 @@ st.subheader("Set Up Your Business in Singapore")
 st.image("./Flowcharts/Set Up Your Business in Singapore.jpg")
 
 st.subheader("Find Government Support")
-st.image("https://drive.google.com/file/d/1dPOTzeTJS1TaD6LjxiLPZiKD2wTSk3uA/view?usp=drive_link")
+st.image("./Flowcharts/Find Government Support.jpg")
 
 st.subheader("Explore EDB Incentives and Facilitation")
-st.image("https://drive.google.com/file/d/1kaxAV3uSttxMTqaRlreeBdoCowx_Wr6O/view?usp=drive_link")
+st.image("./Flowcharts/Explore EDB incentives and facilitation.jpg")
